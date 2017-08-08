1b83cb05a6e871a317c009406a217ba8f5b487db
BLANK\blank <blankshsh@qq.com>
2017-08-08 23:42:38 +0800

    更新脚本 新增readme

---
+++ README.md
@@ -0,0 +1,38 @@
+build from gulp watch
+===
+
+Install
+---
+
+```
+npm install
+```
+
+构造组件
+---
+
+- [gulp]
+- [gulp-livereload]
+- [gulp-postcss]
+- [gulp-less]
+- [gulp-watch]
+- [gulp-minify-css]
+- [gulp-concat]
+- [gulp-uglify]
+- [gulp-rename]
+- [autoprefixer]
+- [css-mqpacker]
+- [csswring]
+- [del]
+- [path]
+
+
+Usage
+---
+
+动态刷新需要sublime chrome安装LiveReload
+
+```
+gulp  // 调用
+gulp watch  // 动态监控
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,33 @@
-var gulp = require('gulp')
-var livereload = require('gulp-livereload')
-var postcss = require('gulp-postcss')
-var autoprefixer = require('autoprefixer')
-var mqpacker = require('css-mqpacker')
-var csswring = require('csswring')
-var less = require('gulp-less')
-var path = require('path')
-var watch = require('gulp-watch')
-var minifycss = require('gulp-minify-css')
-var concat = require('gulp-concat')
-var uglify = require('gulp-uglify')
-var rename = require('gulp-rename')
-var del = require('del')
+/** build by blankshsh 08/08/2017 **/
+
+//引用依赖
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
+var csswring = require('csswring');
+var less = require('gulp-less');
+var path = require('path');
+var watch = require('gulp-watch');
+var minifycss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var del = require('del');
+
+//文件路径
+var lessSrc = './build/less/*.less';
+var cssSrc = './build/css';
+var jsSrc = './build/js/*.js';
+var jsMain = './build/js/main';
+var jsMin = './build/js/min';
 
 gulp.task('default', function() {
   gulp.start('less');
   // gulp.start('less', "js");
 });
+
 gulp.task("less", function() {
   var processors = [
     autoprefixer({
@@ -25,30 +36,31 @@ gulp.task("less", function() {
     mqpacker,
     csswring
   ];
-  return gulp.src('./build/less/*.less') //less的路径
+  return gulp.src(lessSrc) //less路径
     .pipe(less())
     .pipe(postcss(processors))
-    .pipe(gulp.dest('./build/css')) //输出css的路径
+    .pipe(gulp.dest(cssSrc)) //输出css路径
     .pipe(livereload())
 });
+
 gulp.task('js', function() {
-  return gulp.src('./build/js/*.js')
-    // .pipe(concat('main.js')) // 合并所有js到main.js
-    // .pipe(gulp.dest('./build/js/main')) // 输出main.js的路径
+  return gulp.src(jsSrc)
+    // .pipe(concat('main.js')) // 合并js至main
+    // .pipe(gulp.dest(jsMain)) // 输出main路径
     .pipe(rename({
-      suffix: '.min' // 为JS加上min
+      suffix: '.min' // 压缩名称min
     })) 
     .pipe(uglify()) // 压缩JS
-    .pipe(gulp.dest('./build/js/min')) // 输出压缩后的JS
+    .pipe(gulp.dest(jsMin)) // 输出压缩后的JS
     .pipe(livereload()) // 刷新网页
 });
 
 gulp.task('watch', function() {
   livereload.listen();
-  watch('build/js/*.js', function() {
-    gulp.start('lesscss');
-  })
-  watch('build/less/*.less', function() {
-    gulp.start('lesscss');
+  // watch(jsSrc, function() {
+  //   gulp.start('js');
+  // })
+  watch(lessSrc, function() {
+    gulp.start('less');
   })
 })
