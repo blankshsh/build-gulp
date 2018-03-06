05ec6d13e58bea4604e43e7540c50bd7c0ac80ee
blank <blankshsh@qq.com>
2018-03-06 17:19:45 +0800

    add wxss

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
 /** build by blankshsh 08/08/2017 **/
 
 //引用依赖
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var postcss = require('gulp-postcss'); //css后处理器
-var autoprefixer = require('autoprefixer'); // css浏览器兼容
-var mqpacker = require('css-mqpacker'); // 合并css
-var csswring = require('csswring'); // 去空格
-var less = require('gulp-less');
-var watch = require('gulp-watch');
-var concat = require('gulp-concat'); // js合并
-var uglify = require('gulp-uglify'); // js压缩
-var rename = require('gulp-rename'); // 重命名
+var gulp = require('gulp')
+var livereload = require('gulp-livereload')
+var postcss = require('gulp-postcss') //css后处理器
+var autoprefixer = require('autoprefixer') // css浏览器兼容
+var mqpacker = require('css-mqpacker') // 合并css
+var csswring = require('csswring') // 去空格
+var less = require('gulp-less')
+var watch = require('gulp-watch')
+var concat = require('gulp-concat') // js合并
+var uglify = require('gulp-uglify') // js压缩
+var rename = require('gulp-rename') // 重命名
 
 //文件路径
-var lessSrc = './build/less/*.less';
-var cssSrc = './build/css';
-var jsSrc = './build/js/*.js';
-var jsMain = './build/js/main';
-var jsMin = './build/js/min';
+var lessSrc = './build/less/*.less'
+var cssSrc = './build/css'
+var jsSrc = './build/js/*.js'
+var jsMain = './build/js/main'
+var jsMin = './build/js/min'
 
 gulp.task('default', function() {
   // gulp.start('less');
@@ -37,6 +37,8 @@ gulp.task("less", function() {
     .pipe(less())
     .pipe(postcss(processors))
     .pipe(gulp.dest(cssSrc)) //输出css
+    .pipe(rename({ extname: '.wxss' }))
+    .pipe(gulp.dest(wxSrc))
     .pipe(livereload())
 });
 
@@ -46,7 +48,7 @@ gulp.task('js', function() {
     // .pipe(gulp.dest(jsMain)) // 输出main
     .pipe(rename({
       suffix: '.min'
-    })) 
+    }))
     .pipe(uglify())
     .pipe(gulp.dest(jsMin)) // 输出JS
     .pipe(livereload()) // 刷新网页
